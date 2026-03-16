34c81d98560d4606f9bd2b25f83ac9cf8d6fb7c1
Yakeen-coder <4482051@myuwc.ac.za>
2026-03-16 14:40:49 +0200

    added the file

--- 11prac/Prac1.java
+++ 11prac/Prac1.java
@@ -6,22 +6,22 @@ public class Prac1 {
         int[] shuffled = new int[N + 1];
         boolean[] isNotPresent = new boolean[N + 1];
         for (int i = 1; i <= N; i++) {
-            isNotPresent[i] = true; [cite: 22, 24]
+            isNotPresent[i] = true; 
         }
         
-        int i = 0; [cite: 30]
-        while (i < N - 1) { [cite: 27, 41]
-            int r = (int)(Math.random() * N) + 1; [cite: 19, 40]
-            if (isNotPresent[r]) { [cite: 29]
-                i += 1; [cite: 30]
-                shuffled[i] = r; [cite: 31]
-                isNotPresent[r] = false; [cite: 39]
+        int i = 0; 
+        while (i < N - 1) { 
+            int r = (int)(Math.random() * N) + 1; 
+            if (isNotPresent[r]) { 
+                i += 1; 
+                shuffled[i] = r; 
+                isNotPresent[r] = false; 
             }
         }
         
-        for (int r = 1; r <= N; r++) { [cite: 42]
-            if (isNotPresent[r]) { [cite: 42]
-                shuffled[N] = r; [cite: 42]
+        for (int r = 1; r <= N; r++) { 
+            if (isNotPresent[r]) { 
+                shuffled[N] = r; 
                 break;
             }
         }
@@ -29,59 +29,61 @@ public class Prac1 {
         return Arrays.copyOfRange(shuffled, 1, N + 1);
     }
 
-    public static int[] biasedshuffle(int N) {
+    public static int[] biasedshuffle(int N){
         int[] shuffled = new int[N];
         for (int i = 0; i < N; i++) {
-            shuffled[i] = i + 1; [cite: 45, 46]
+            shuffled[i] = i + 1;  
+    }
+        int[] shuffled = new int[N];
+        for (int i = 0; i < N; i++) {
+            shuffled[i] = i + 1; 
         }
         
         for (int i = 0; i < N; i++) {
-            int r = (int)(Math.random() * N); [cite: 46]
+            int r = (int)(Math.random() * N); 
             int temp = shuffled[i];
-            shuffled[i] = shuffled[r]; [cite: 46]
-            shuffled[r] = temp; [cite: 46]
-        }
+            shuffled[i] = shuffled[r]; 
+            shuffled[r] = temp; 
         return shuffled;
     }
 
     public static int[] shuffle(int N) {
         int[] B = new int[N];
         for (int i = 0; i < N; i++) {
-            B[i] = i + 1; [cite: 52]
+            B[i] = i + 1; 
         }
         
-        int b = 0; [cite: 53]
-        while (b < N) { [cite: 54]
-            int r = b + (int)(Math.random() * (N - b)); [cite: 55]
+        int b = 0; 
+        while (b < N) { 
+            int r = b + (int)(Math.random() * (N - b)); 
             int temp = B[b];
-            B[b] = B[r]; [cite: 55]
-            B[r] = temp; [cite: 55]
-            b++; [cite: 56]
+            B[b] = B[r]; 
+            B[r] = temp; 
+            b++;
         }
         return B;
     }
 
     public static void runBiasTest(String mode) {
-        Map<String, Integer> D = new HashMap<>(); [cite: 64]
+        Map<String, Integer> D = new HashMap<>(); 
         
-        for (int i = 0; i < 60000; i++) { [cite: 57]
+        for (int i = 0; i < 60000; i++) { 
             int[] result = mode.equals("biased") ? biasedshuffle(3) : shuffle(3); [cite: 57]
             StringBuilder key = new StringBuilder();
             for (int val : result) {
-                key.append(val); [cite: 63]
+                key.append(val); 
             }
             String k = key.toString();
             
-            if (!D.containsKey(k)) { [cite: 67]
-                D.put(k, 1); [cite: 69]
-            } else { [cite: 71]
-                D.put(k, D.get(k) + 1); [cite: 73]
+            if (!D.containsKey(k)) { 
+                D.put(k, 1); 
+            } else { 
+                D.put(k, D.get(k) + 1); 
             }
         }
         
-        for (String key : new TreeSet<>(D.keySet())) { [cite: 78]
-            System.out.println(key + " " + D.get(key)); [cite: 80]
-        }
+        for (String key : new TreeSet<>(D.keySet())) {
+            System.out.println(key + " " + D.get(key)); 
     }
 
     public static void main(String[] args) {
---
+++ tryHeapsort.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class tryHeapsort {
+    public void bottomUp( int[] arr){
+        int n = arr.length;
+        for()
+    }
+    
+}
