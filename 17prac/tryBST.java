c98fb097b9e7400a41749f4beda46bf9348b3c6a
Yakeen-coder <4482051@myuwc.ac.za>
2026-03-23 15:00:37 +0200

    Testing once again

--- 17prac/tryBST.java
+++ 17prac/tryBST.java
@@ -1,3 +1,40 @@
+import java.util.*;
+
 public class tryBST {
     
-}
+    static class tNode {
+        int value;
+        tNode left;
+        tNode right;
+        
+        tNode(int value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    
+    private tNode root;
+    
+    public tryBST() {
+        this.root = null;
+    }
+    
+    public void insert(int value) {
+        root = insertHelper(root, value);
+    }
+    
+    private tNode insertHelper(tNode node, int value) {
+        if (node == null) {
+            return new tNode(value);
+        }
+        
+        if (value < node.value) {
+            node.left = insertHelper(node.left, value);
+        } else if (value > node.value) {
+            node.right = insertHelper(node.right, value);
+        }
+        
+        return node;
+    }
+    
