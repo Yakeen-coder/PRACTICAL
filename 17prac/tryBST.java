c4c729c011cb96087227c71a4e7dd2fd3018804e
Yakeen-coder <4482051@myuwc.ac.za>
2026-03-23 15:09:15 +0200

    Added changes, git working :-)

--- 17prac/tryBST.java
+++ 17prac/tryBST.java
@@ -37,4 +37,35 @@ public class tryBST {
         
         return node;
     }
+
+    public void delete(int value) {
+        root = deleteHelper(root, value);
+    }
+    
+    private tNode deleteHelper(tNode node, int value) {
+        if (node == null) {
+            return null;
+        }
+
+        if (value < node.value) {
+            node.left = deleteHelper(node.left, value);
+        } else if (value > node.value) {
+            node.right = deleteHelper(node.right, value);
+        } else {
+            if (node.left == null && node.right == null) {
+                return null;
+            }
+            if (node.left == null) {
+                return node.right;
+            }
+            if (node.right == null) {
+                return node.left;
+            }
+            tNode successor = findMin(node.right);
+            node.value = successor.value;
+            node.right = deleteHelper(node.right, successor.value);
+        }
+        
+        return node; 
+
     
